1210fce393dfb256d97174f21f8c93ec44b8069b
tsunga59 <tatsumochi15@gmail.com>
2024-03-26 22:47:45 +0900

    containerで囲む

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,49 +3,53 @@
 
 <template>
   <div id="about" class="about">
-    <p class="main-text">
-      <span class="bold">「秘めたまま、終わるな。」</span>
-      <br />
-      先生にも友達にも家族にも言えていない「本当は目指したい」「本当は叶えたい」
-      <br />
-      そんな心の底から湧き上がる夢を、君だけが知っているはず。
-      <br />
-      <br />
-      誰もが夢や野心を持っていて、それを誰かに伝えられるわけではない。
-      <br />
-      だけど、夢は、見るものではなく、実現させるもの。
-      <br />
-      僕たちは、生徒の<span class="bold">"できたらいいな"</span>を、確かな<span class="bold">"できる"</span>に変える。
-      <br />
-      <br />
-      私たちのミッションは、生徒が持つ夢や願いを形にすること。
-      <br />
-      君の<span class="bold">"したい"</span>がここで<span class="bold">"できる"</span>に変わる瞬間を、私たちは全力でサポートする。
-      <br />
-      受験勉強を超えた、<span class="bold">『夢を実現する力』</span>を。
-    </p>
-    <p class="sub-text">
-      生徒1人につき、受験専門のプロコーチ3人が、
-      <br />
-      君に専属で勉強の根本から<span class="bold">"考える力"</span>を教える。
-      <br />
-      <br />
-      先生や授業を超え、自分で学び成長する力を身につけさせる。
-      <br />
-      ただ覚えるだけの勉強から脱却させ、<span class="bold">"考える力"</span>を根本から育てる。
-      <br />
-      <br />
-      自分自身で学び、成長する力を、君だけのために。
-    </p>
+    <div class="container">
+      <p class="main-text">
+        <span class="bold">「秘めたまま、終わるな。」</span>
+        <br />
+        先生にも友達にも家族にも言えていない「本当は目指したい」「本当は叶えたい」
+        <br />
+        そんな心の底から湧き上がる夢を、君だけが知っているはず。
+        <br />
+        <br />
+        誰もが夢や野心を持っていて、それを誰かに伝えられるわけではない。
+        <br />
+        だけど、夢は、見るものではなく、実現させるもの。
+        <br />
+        僕たちは、生徒の<span class="bold">"できたらいいな"</span>を、確かな<span class="bold">"できる"</span>に変える。
+        <br />
+        <br />
+        私たちのミッションは、生徒が持つ夢や願いを形にすること。
+        <br />
+        君の<span class="bold">"したい"</span>がここで<span class="bold">"できる"</span>に変わる瞬間を、私たちは全力でサポートする。
+        <br />
+        受験勉強を超えた、<span class="bold">『夢を実現する力』</span>を。
+      </p>
+      <p class="sub-text">
+        生徒1人につき、受験専門のプロコーチ3人が、
+        <br />
+        君に専属で勉強の根本から<span class="bold">"考える力"</span>を教える。
+        <br />
+        <br />
+        先生や授業を超え、自分で学び成長する力を身につけさせる。
+        <br />
+        ただ覚えるだけの勉強から脱却させ、<span class="bold">"考える力"</span>を根本から育てる。
+        <br />
+        <br />
+        自分自身で学び、成長する力を、君だけのために。
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .about {
   text-align: center;
-  width: 85%;
-  padding: 2rem;
-  margin: 6rem auto;
+}
+.container {
+  width: 100%;
+  padding: 6rem;
+  box-sizing: border-box;
 }
 .main-text {
   color: #000;
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -3,56 +3,59 @@
 
 <template>
   <div id="work" class="work">
-    <h2 class="sub_title">Work</h2>
-    <ul class="work_area">
-      <li class="work_list">
-        <div class="img_area">
-          Image
-        </div>
-        <div class="text_area">
-          <p class="work_title">Title</p>
-          <p class="work_text">
-            Text Text Text
-          </p>
-        </div>
-      </li>
-      <li class="work_list">
-        <div class="img_area">
-          Image
-        </div>
-        <div class="text_area">
-          <p class="work_title">Title</p>
-          <p class="work_text">
-            Text Text Text
-          </p>
-        </div>
-      </li>
-      <li class="work_list">
-        <div class="img_area">
-          Image
-        </div>
-        <div class="text_area">
-          <p class="work_title">Title</p>
-          <p class="work_text">
-            Text Text Text
-          </p>
-        </div>
-      </li>
-    </ul>
+    <div class="container">
+      <h2 class="sub_title">Work</h2>
+      <ul class="work_area">
+        <li class="work_list">
+          <div class="img_area">
+            Image
+          </div>
+          <div class="text_area">
+            <p class="work_title">Title</p>
+            <p class="work_text">
+              Text Text Text
+            </p>
+          </div>
+        </li>
+        <li class="work_list">
+          <div class="img_area">
+            Image
+          </div>
+          <div class="text_area">
+            <p class="work_title">Title</p>
+            <p class="work_text">
+              Text Text Text
+            </p>
+          </div>
+        </li>
+        <li class="work_list">
+          <div class="img_area">
+            Image
+          </div>
+          <div class="text_area">
+            <p class="work_title">Title</p>
+            <p class="work_text">
+              Text Text Text
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .work {
   text-align: center;
-  width: 85%;
-  padding: 2rem;
-  margin: 6rem auto;
+}
+.container {
+  width: 100%;
+  padding: 6rem;
+  box-sizing: border-box;
 }
 .sub_title {
   font-size: 2rem;
   letter-spacing: .2rem;
-  color: #000;
   margin-bottom: 4rem;
 }
 .work_area {
@@ -62,7 +65,7 @@
 }
 .work_list {
   width: 33%;
-  margin: auto 2rem;
+  margin: auto 1rem;
   .img_area {
     display: flex;
     justify-content: center;
